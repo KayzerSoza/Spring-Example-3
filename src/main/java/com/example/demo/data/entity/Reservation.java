958cd507a2b90e717c59087c19b5f121a31881f7
Husamettin Arslan <husamettinarslan@hotmail.com>
2020-02-24 11:29:39 +0100

    Guest, GuestRepository, Reservation and ReservationRepository classes created. Controller classes added accordingly

---
+++ src/main/java/com/example/demo/data/entity/Reservation.java
@@ -0,0 +1,22 @@
+package com.example.demo.data.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name="RESERVATION")
+public class Reservation {
+  @Id
+  @Column(name="RESERVATION_ID")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long reservationId;
+
+  @Column(name="ROOM_ID") private long roomId;
+  @Column(name="GUEST_ID") private long guestId;
+  @Column(name="RES_DATE") private Date date;
+
+}
--- src/main/java/com/example/demo/data/entity/Room.java
+++ src/main/java/com/example/demo/data/entity/Room.java
@@ -1,8 +1,10 @@
 package com.example.demo.data.entity;
 
 
-import javax.persistence.*;
+import lombok.Data;
 
+import javax.persistence.*;
+@Data
 @Entity
 @Table(name="ROOM")
 public class Room {
@@ -17,35 +19,5 @@ public class Room {
   @Column(name="BED_INFO")
   private String bedInfo;
 
-  public long getRoomId() {
-    return roomId;
-  }
 
-  public void setRoomId(long roomId) {
-    this.roomId = roomId;
-  }
-
-  public String getRoomName() {
-    return roomName;
-  }
-
-  public void setRoomName(String roomName) {
-    this.roomName = roomName;
-  }
-
-  public String getRoomNumber() {
-    return roomNumber;
-  }
-
-  public void setRoomNumber(String roomNumber) {
-    this.roomNumber = roomNumber;
-  }
-
-  public String getBedInfo() {
-    return bedInfo;
-  }
-
-  public void setBedInfo(String bedInfo) {
-    this.bedInfo = bedInfo;
-  }
 }
